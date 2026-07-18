2fa352e856a7ce03075928e9e05b4705189d4c82
coconut-696 <coconut8568@gmail.com>
2026-07-18 21:43:41 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
-.
+from telegram import Update
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    MessageHandler,
+    ContextTypes,
+    filters,
+)
+
+TOKEN = "8726662807:AAHua1G32iWYbcFzC_CfvUU49P3B971n1GY"
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "سلام 👋\nبه ربات پیام ناشناس خوش آمدی.\nپیامت را بفرست."
+    )
+  async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "پیامت دریافت شد ✅\nبه‌زودی ارسال می‌شود."
+    )
+    def main():
+    app = Application.builder().token(TOKEN).build()
+
+    app.add_handler(CommandHandler("start", start))
+
+    app.add_handler(
+        MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message)
+    )
+
+    app.run_polling()
+
+
+if __name__ == "__main__":
+    main()
