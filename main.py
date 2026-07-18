45cab8fd3cd286cc56193bc5b8d52d6ce93ce4f4
coconut-696 <coconut8568@gmail.com>
2026-07-18 22:20:03 +0330

    Update main.py

--- main.py
+++ main.py
@@ -6,23 +6,41 @@ from telegram.ext import (
     ContextTypes,
     filters,
 )
+
 TOKEN = "8726662807:AAElPtZ8HpqK7uMm0P8TKY-BZ0O1OzObAaM"
 ADMIN_ID = 7421114211
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "سلام 👋\nپیامت را بفرست."
+    )
+
+
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user_message = update.message.text
+
     await context.bot.send_message(
         chat_id=ADMIN_ID,
         text=f"📩 پیام جدید:\n\n{user_message}"
     )
+
     await update.message.reply_text(
         "پیامت ارسال شد ✅"
     )
+
+
 def main():
     app = Application.builder().token(TOKEN).build()
+
     app.add_handler(CommandHandler("start", start))
+
     app.add_handler(
         MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message)
     )
+
     app.run_polling()
+
+
 if __name__ == "__main__":
     main()
