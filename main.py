55e59ffd0dd6c06d8d28256fad081a50123e8b70
coconut-696 <coconut8568@gmail.com>
2026-07-18 23:00:35 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from telegram import Update
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import (
     Application,
     CommandHandler,
     MessageHandler,
+    CallbackQueryHandler,
     ContextTypes,
     filters,
 )
@@ -10,44 +11,92 @@ from telegram.ext import (
 TOKEN = "8726662807:AAEhyxd8epMeVuI82xT1_bCRCXXHUWChkCE"
 ADMIN_ID = 7421114211
 
+# ذخیره ارتباط کاربرها (موقت)
+users = {}
+
+# کاربرها و حالت پاسخ دادن ادمین
+reply_mode = None
+
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
         "سلام 👋\n"
-        "به ربات پیام ناشناس خوش آمدی.\n"
-        "پیامت را بفرست."
+        "پیامت را بفرست. پیام تو ناشناس ارسال می‌شود."
     )
 
 
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    user_message = update.message.text
+    global reply_mode
 
-    try:
+    user_id = update.effective_user.id
+    text = update.message.text
+
+    # اگر ادمین در حالت پاسخ باشد
+    if update.effective_user.id == ADMIN_ID and reply_mode:
+        target_user = reply_mode
+
         await context.bot.send_message(
-            chat_id=ADMIN_ID,
-            text=(
-                "📩 پیام جدید دریافت شد:\n\n"
-                f"{user_message}"
-            )
+            chat_id=target_user,
+            text=f"📩 پاسخ:\n\n{text}"
         )
+
+        reply_mode = None
 
         await update.message.reply_text(
-            "پیامت ارسال شد ✅"
+            "پاسخ ارسال شد ✅"
         )
+        return
 
-    except Exception as e:
-        print("ERROR:", e)
+    # پیام کاربر معمولی
+    users[user_id] = True
 
-        await update.message.reply_text(
-            "خطا در ارسال پیام ❌"
-        )
+    button = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    "پاسخ به کاربر",
+                    callback_data=f"reply_{user_id}"
+                )
+            ]
+        ]
+    )
+
+    await context.bot.send_message(
+        chat_id=ADMIN_ID,
+        text=f"📩 پیام جدید:\n\n{text}",
+        reply_markup=button
+    )
+
+    await update.message.reply_text(
+        "پیامت ارسال شد ✅"
+    )
+
+
+async def reply_button(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    global reply_mode
+
+    query = update.callback_query
+    await query.answer()
+
+    if query.from_user.id != ADMIN_ID:
+        return
+
+    user_id = int(query.data.split("_")[1])
+
+    reply_mode = user_id
+
+    await query.message.reply_text(
+        "✍️ پاسخ خود را بنویس:"
+    )
 
 
 def main():
     app = Application.builder().token(TOKEN).build()
 
+    app.add_handler(CommandHandler("start", start))
+
     app.add_handler(
-        CommandHandler("start", start)
+        CallbackQueryHandler(reply_button)
     )
 
     app.add_handler(
